5796eb86da7df9fb9abdb743255118423321ddad
hind0025 <s.hindveer2004@gmail.com>
2024-11-12 11:55:25 +0530

    fetching details

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const router = express.Router();
-//import {generateToken} from "../middleware/jwtMiddleware";
-// const{ validatejwtToken,generateToken}=require("../middleware/jwtMiddleware")
+const asyncHandler=require("express-async-handler")
+const {validatejwtToken}=require("../middleware/jwtMiddleware")
+
+const User=require("../model/userModel")
+const myacc=async(req,res)=>
+{
+    const{email}=req.body;
+    const user=await User.findOne({email});
+    if(user)
+    {
+        res.send(user);
+    }
+
+}
 const {
     registerUser,
      loginUser
 }=require("../controller/userController");
 router.post("/register" , registerUser);
 router.post("/login",loginUser);
+router.get("/details",myacc,validatejwtToken)
 // router.post("/protected", jwtAuthMiddleware, (req, res) => {
 //     res.json({ message: "Access granted to protected resource", user: req.user });
 // });
